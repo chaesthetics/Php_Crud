964683fc525de60ffe53871647fa8740edb4d90c
Auriel James Fernandez <61530799+chaesthetics@users.noreply.github.com>
2023-06-09 20:14:04 +0800

    Add files via upload

---
+++ crud (1).sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 09, 2023 at 02:13 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `sid` int(11) NOT NULL,
+  `sname` varchar(100) NOT NULL,
+  `saddress` varchar(150) NOT NULL,
+  `sclass` int(10) NOT NULL,
+  `sphone` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`sid`, `sname`, `saddress`, `sclass`, `sphone`) VALUES
+(62, 'Mina Tozaki', 'koreaaa', 0, '0923094345'),
+(70, 'Mama Leni', 'Alabang', 0, '35'),
+(71, 'Wala', 'Diko alam ', 0, '09209021'),
+(73, 'Sana ', 'KOrea aa', 0, '0909090909'),
+(74, 'sasa', 'asas', 0, '23432'),
+(76, 'POTPOT', 'eqe', 1, 'qew');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studentclass`
+--
+
+CREATE TABLE `studentclass` (
+  `cid` int(11) NOT NULL,
+  `cname` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `studentclass`
+--
+
+INSERT INTO `studentclass` (`cid`, `cname`) VALUES
+(1, 'BSIT'),
+(2, 'BSBA'),
+(3, 'BSCS'),
+(4, 'BetLOG');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`sid`);
+
+--
+-- Indexes for table `studentclass`
+--
+ALTER TABLE `studentclass`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+
+--
+-- AUTO_INCREMENT for table `studentclass`
+--
+ALTER TABLE `studentclass`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
